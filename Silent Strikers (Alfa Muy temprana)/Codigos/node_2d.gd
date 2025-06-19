b905e125cbf0f0c3b67aa9b95872b2276643cd6f
Garcia-IGC <ignacio.garcia02@alumnos.ucn.cl>
2025-06-19 17:55:33 -0400

    copia y pega, funcionamiento actual, okey

--- Silent Strikers (Alfa Muy temprana)/Codigos/node_2d.gd
+++ Silent Strikers (Alfa Muy temprana)/Codigos/node_2d.gd
@@ -1,11 +1,11 @@
 extends Node2D  # o SubViewport si lo ponés ahí
 
 @onready var jugador_icono = $jugador_icono
-@onready var enemigo_icono = $guardia_icono
-@onready var jugador = get_node("/root/Mapa de Prueba/Ladron")
-@onready var enemigo = get_node("/root/Mapa de Prueba/Guardia 2")  # Cambiá si es distinto
+@onready var guardia_icono = $guardia_icono
+@onready var jugador = get_node("/root/TestMapa1/CharacterBody2D")  # Cambiá si es distinto
 
 func _process(_delta):
 	jugador_icono.global_position = jugador.global_position
-	enemigo_icono.global_position = enemigo.global_position
 	
+	for guardia in get_tree().get_nodes_in_group("Guardias"):
+		guardia_icono.global_position = guardia.global_position
